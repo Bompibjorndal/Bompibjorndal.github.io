75c7d16936234e9c59cb5a8e994e338dde7f6e44
Bompibjorndal <e-henbjo1@iskule.no>
2024-12-09 14:00:40 +0100

    Frosker

---
+++ Frosker/kode.js
@@ -0,0 +1,9 @@
+let lilje=document.getElementById ("frosk_lilje.jpg");
+let mindreverdt=document.getElementById ("frosk_mindreverdt.webp");
+let klatre=document.getElementById ("klatre_frosk.wepb");
+let padde=document.getElementById("Padde_oransje.jpg");
+
+let BilderListe=[lilje, mindreverdt, klatre, padde]
+
+
+addEventListener
